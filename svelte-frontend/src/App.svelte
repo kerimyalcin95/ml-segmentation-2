bd823a0aab97be55e88782119626508a6a369570
Kerim Yalcin <kerimyalcin.95@it-yalc.in>
2026-03-15 10:54:03 +0100

    feat: add custom theme from skeleton

--- svelte-frontend/src/App.svelte
+++ svelte-frontend/src/App.svelte
@@ -18,5 +18,8 @@
     }
 </script>
 
-<button on:click={increment}>Increment</button>
-<p>{$count} + 1000: {$message}</p>
+<main class="flex flex-col items-center justify-center min-h-screen gap-4 bg-base-100 p-4">
+    <button class="btn bg-primary-500 " on:click={increment}>Increment</button>
+    <p class="outline rounded justify-start bg-primary-500 text-primary-contrast-500 p-4">{$count} + 1000: {$message}</p>
+</main>
+
